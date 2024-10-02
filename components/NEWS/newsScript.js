36a62dae5df8c26235b6c23ce1db4b94c99f1010
Birbal Kumar <153169574+birbalkr@users.noreply.github.com>
2024-10-02 11:22:10 +0530

    Update newsScript.js api

--- components/NEWS/newsScript.js
+++ components/NEWS/newsScript.js
@@ -2,7 +2,7 @@ const imgItem = document.getElementById('imgItem');
 const tailwindcard = document.getElementById('tailwindcard');
 const carouselIndicators = document.getElementById('carouselIndicators');
 
-const API_URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5a3401ab7ff2433ebf3773976c206834';
+const API_URL = 'https://newsdata.io/api/1/latest?apikey=pub_5507661984d1b35d983b103b0e36c2f70927c&category=politics&country=bd';
 const API_URL_tech = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=5a3401ab7ff2433ebf3773976c206834';
 
 
