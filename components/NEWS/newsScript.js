f0176fcdc19f75ad7d17dc385eff629dbfa74a65
birbalkr <birbalkr1435@gmail.com>
2024-10-06 14:07:26 +0530

    add more button

--- components/NEWS/newsScript.js
+++ components/NEWS/newsScript.js
@@ -3,7 +3,7 @@ const tailwindcard = document.getElementById('tailwindcard');
 const carouselIndicators = document.getElementById('carouselIndicators');
 
 const API_URL = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=5a3401ab7ff2433ebf3773976c206834';
-const API_URL_tech = 'https://newsdata.io/api/1/latest?apikey=pub_5507661984d1b35d983b103b0e36c2f70927c&category=politics&country=bd';
+const API_URL_tech = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=5a3401ab7ff2433ebf3773976c206834';
 
 
 async function fetchNews() {
@@ -87,18 +87,18 @@ async function fetchTech() {
         console.log(data);
 
 
-        if (!data.results || data.results.length === 0) {
+        if (!data.articles || data.articles.length === 0) {
             imgItem.innerHTML = "No articles found.";
             return;
         }
 
         // Populate the carousel 
-        for (let i = 0; i < Math.min(data.results.length, 2); i++) {
-            displayCard(data.results[i], i === 0);
+        for (let i = 0; i < Math.min(data.articles.length, 2); i++) {
+            displayCard(data.articles[i], i === 0);
 
         }
-        for (let i = 0; i < Math.min(data.results.length, 9); i++) {
-            displaytechCard(data.results[i]);
+        for (let i = 0; i < Math.min(data.articles.length, 9); i++) {
+            displaytechCard(data.articles[i]);
 
         }
         displayNewsCard
@@ -115,9 +115,9 @@ function displayCard(imageSrc) {
     const h5 = document.createElement('h5');
     const p = document.createElement('p');
     a.className = "flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700";
-    a.href = imageSrc.source_url;
+    a.href = imageSrc.url;
     image.className = 'object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg';
-    image.src = imageSrc.image_url;
+    image.src = imageSrc.urlToImage;
     div.className = "flex flex-col justify-between p-4 leading-normal";
     h5.className = "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white";
     h5.textContent = imageSrc.title;
@@ -133,53 +133,84 @@ function displayCard(imageSrc) {
 
 }
 
+
 function displaytechCard(imageSrc) {
+    // create Element 
     const a =document.createElement('a');
+    const a1 =document.createElement('a');
+    const button = document.createElement('button');
     const div= document.createElement('div');
-    a.href=imageSrc.url;
     const image = document.createElement('img')
-    image.src=imageSrc.image_url;
-    image.className="rounded-t-lg";
-    a.appendChild(image);
-    div.className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";
-    div.appendChild(a);
-    tech5Card.appendChild(div)
-
-    div.className="p-5";
-    a.href=imageSrc.url;
     const h5=document.createElement('h5');
-    h5.className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white";
-    h5.textContent=imageSrc.title;
-    a.appendChild(h5);
 
+    // class of Element 
+    image.className="rounded-t-lg";
+    div.className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";
+    div.className="p-5";
+    h5.className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white";
+    button.className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full';
+    
+    // add link 
+    a.href=imageSrc.url;
+    image.src=imageSrc.urlToImage;
+    a.href=imageSrc.url;
+    a1.href=imageSrc.url;
+    
+    // appendChild
+    a.appendChild(image);
     div.appendChild(a);
-    tech5Card.appendChild(div);
+    a.appendChild(h5);
+    div.appendChild(a);
+    button.appendChild(a1);
+    div.appendChild(button);
+    
+    h5.textContent=imageSrc.title;
+    button.type = "button";
+    a1.textContent="Read More";
+
+    // add in html page 
+
+    News5Card.appendChild(div)
+    News5Card.appendChild(div);
 }
 
 function displayNewsCard(imageSrc) {
+    // create Element 
     const a =document.createElement('a');
+    const a1 =document.createElement('a');
     const button = document.createElement('button');
     const div= document.createElement('div');
-    a.href=imageSrc.url;
     const image = document.createElement('img')
-    image.src=imageSrc.urlToImage;
-    image.className="rounded-t-lg";
-    a.appendChild(image);
-    div.className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";
-    div.appendChild(a);
-    News5Card.appendChild(div)
-
-    div.className="p-5";
-    a.href=imageSrc.url;
     const h5=document.createElement('h5');
+
+    // class of Element 
+    image.className="rounded-t-lg";
+    div.className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";
+    div.className="p-5";
     h5.className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white";
-    h5.textContent=imageSrc.title;
+    button.className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full';
+    
+    // add link 
+    a.href=imageSrc.url;
+    image.src=imageSrc.urlToImage;
+    a.href=imageSrc.url;
+    a1.href=imageSrc.url;
+    
+    // appendChild
+    a.appendChild(image);
+    div.appendChild(a);
     a.appendChild(h5);
     div.appendChild(a);
-    button.type = "button";
-    a.textContent="Read More";
-    button.appendChild(a);
+    button.appendChild(a1);
     div.appendChild(button);
+    
+    h5.textContent=imageSrc.title;
+    button.type = "button";
+    a1.textContent="Read More";
+
+    // add in html page 
+
+    News5Card.appendChild(div)
     News5Card.appendChild(div);
 }
 
